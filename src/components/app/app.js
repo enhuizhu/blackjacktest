5c15bd46ad3c6792bb8a9c1180f06c562d07e889
Enhui Zhu <enhuizhu@Enhuis-MacBook-Pro.local>
2017-10-18 16:13:31 +0100

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,24 @@
+import React, {PropTypes} from 'react';
+
+import Button from '../button';
+import Card from '../card';
+import Text from '../text';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Button>Primary</Button>
+      <Button color="tertiary">Secondary button</Button>
+      <Card suit="club" value="jack" />
+      <Card suit="diamond" value="jack" />
+      <Card suit="spade" value="jack" />
+      <Card suit="heart" value="jack" />
+      <Text>Hello world!</Text>
+      <Text size="huge">Hello world!</Text>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/game.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class game {
+  
+
+
+  
+}
+
+export default game;
